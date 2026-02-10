79a3d7fcf341f13c56a532b7ec91640729fedbe5
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-10 11:19:30 +0100

    strutturazione dello scheletro del secondo esercizio

--- main.js
+++ main.js
@@ -30,11 +30,16 @@ Dichiariamo chi ha vinto.
 */
 
 //L’utente sceglie pari o dispari
+
 const user_choice= prompt("even or odd")
+
 //L’utente inserisce un numero da 1 a 5
+
 const user_Number= Number(prompt("choose a number between 1 and 5"))
 console.log(user_choice,user_Number);
+
 //Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+
 function getRndInteger(min , max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
@@ -43,23 +48,20 @@ let pc_number = getRndInteger(1, 5);
 console.log(pc_number);
 
 //Sommiamo i due numeri
+
 let som_Number = user_Number + pc_number
 console.log(som_Number);
 
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
 function getEvenOrOdd() {
-    
     const is_Even=false;
     if ( som_Number %2==0 ) {
         is_Even="true";
         console.log("Even");
-        
     } 
     else  {
-        
         console.log("Odd");
-    
     } 
     return is_Even
 }
@@ -76,8 +78,32 @@ if (user_choice == "even" && getEvenOrOdd() == true ) {
     
 console.log(message);
 
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
 
 //Chiedere all’utente di inserire una parola
+
 const user_Word=prompt("inserisci una parola")
 console.log(user_Word);
-//Creare una funzione per capire se la parola inserita è palindroma
+
+//Creare una variabile per salvare la parola dell'utente
+//[split]la porla in una sequenza di caratteri in un array
+//creare un loop per ogni interazione 
+//prendi il character e push nello split_arrey
+
+//[reverse]l'array del character
+//loop over the array from the end
+//prendi il character e immagazinalo nel reversed array
+
+//[join]the array in una stringa
+//loop over the reversed_array and
+//immagazina character indietro in plain string con strings concatenation
+
+//controllare se la parola reversata e uguale alla parola originale 
+//controllare se e true
+//its a palindrome
+//otherwise
+//non at a palindrome
